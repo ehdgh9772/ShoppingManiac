f84a19f4b006d33735e335fb7a2bb68f57a9ee41
ehdgh9772 <ehdgh9772@naver.com>
2017-07-04 13:39:43 +0900

    Version Control="0.03"

--- app/src/main/java/com/example/kcci/shoppingmaniac/ListContentFragment.java
+++ app/src/main/java/com/example/kcci/shoppingmaniac/ListContentFragment.java
@@ -17,6 +17,7 @@ import android.widget.TextView;
 
 public class ListContentFragment extends Fragment {
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
