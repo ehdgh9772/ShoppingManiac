128625c6de6816916a20c7d24ec612b7f4108a06
ehdgh9772 <ehdgh9772@naver.com>
2017-07-05 17:36:03 +0900

    Version Control="0.06"

--- app/src/main/java/com/example/kcci/shoppingmaniac/MainActivity.java
+++ app/src/main/java/com/example/kcci/shoppingmaniac/MainActivity.java
@@ -83,9 +83,9 @@ public class MainActivity extends AppCompatActivity {
     // Add Fragments to Tabs
     private void setupViewPager(ViewPager viewPager) {
         Adapter adapter = new Adapter(getSupportFragmentManager());
-        adapter.addFragment(new ListContentFragment(), "List");
-        adapter.addFragment(new TileContentFragment(), "Tile");
-        adapter.addFragment(new CardContentFragment(), "Card");
+        //adapter.addFragment(new ListContentFragment(), "List");
+        //adapter.addFragment(new TileContentFragment(), "Tile");
+        adapter.addFragment(new CardContentFragment(), "이마트 화곡역점");
         viewPager.setAdapter(adapter);
     }
 
