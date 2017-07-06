9a7b8ddbe6ff17fb36bc561141d44f48cce8fb9f
ehdgh9772 <ehdgh9772@naver.com>
2017-07-06 17:06:16 +0900

    Version Control = "0.12"

--- shoppingmanager/src/main/java/com/example/shoppingmanager/MainActivity.java
+++ shoppingmanager/src/main/java/com/example/shoppingmanager/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.shoppingmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button imageShooting = (Button) findViewById(R.id.imageShooting);
+        Button listInquiry = (Button) findViewById(R.id.listInquiry);
+        Button registration = (Button) findViewById(R.id.registration);
+
+        imageShooting.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                }
+        );
+
+        listInquiry.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                }
+        );
+
+        registration.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                }
+        );
     }
 }
