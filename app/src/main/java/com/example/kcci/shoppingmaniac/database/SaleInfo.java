d480db8857c59b81e741bddca3e494ae9dca62b3
KooMyeongHoe <mhk9990@gmail.com>
2017-07-06 17:01:13 +0900

    Database 패키지 생성 및 SaleInfo타입 생성

---
+++ app/src/main/java/com/example/kcci/shoppingmaniac/database/SaleInfo.java
@@ -0,0 +1,13 @@
+package com.example.kcci.shoppingmaniac.database;
+
+/**
+ * Created by koo on 17. 7. 6.
+ */
+
+public class SaleInfo {
+    String name;
+    String discountType;
+    String price;
+    String discountedPrice;
+    String category;
+}
