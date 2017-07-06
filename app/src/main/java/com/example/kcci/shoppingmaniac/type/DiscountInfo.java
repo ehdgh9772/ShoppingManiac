9a1b743073db98b2b2910a47bf7d2cbb9449ebe3
KooMyeongHoe <mhk9990@gmail.com>
2017-07-06 18:13:03 +0900

    이름변경, DBM 수정

---
+++ app/src/main/java/com/example/kcci/shoppingmaniac/type/DiscountInfo.java
@@ -0,0 +1,13 @@
+package com.example.kcci.shoppingmaniac.type;
+
+/**
+ * Created by koo on 17. 7. 6.
+ */
+
+public class DiscountInfo {
+    public String name;
+    public String discountType;
+    public String price;
+    public String discountedPrice;
+    public String category;
+}
