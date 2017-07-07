f9a532596df91bfa79cbf29ae112dab18f2d025a
ehdgh9772 <ehdgh9772@naver.com>
2017-07-07 12:37:42 +0900

    Version Control = "0.15"

--- shoppingmanager/src/main/java/com/example/shoppingmanager/ImageShooting.java
+++ shoppingmanager/src/main/java/com/example/shoppingmanager/ImageShooting.java
@@ -1,13 +1,39 @@
 package com.example.shoppingmanager;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class ImageShooting extends AppCompatActivity {
+public class ImageShooting extends AppCompatActivity{
+    Button btn = null;
+    ImageView iv = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_shooting);
+
+        btn = (Button)findViewById(R.id.imageShooting);
+        iv = (ImageView)findViewById(R.id.imageView);
+
+        btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent();
+                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        iv.setImageURI(data.getData());
     }
 }
---
+++ shoppingmanager/src/main/java/com/example/shoppingmanager/MyCameraSurface.java
@@ -0,0 +1,50 @@
+package com.example.shoppingmanager;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+class MyCameraSurface extends SurfaceView implements SurfaceHolder.Callback{
+    SurfaceHolder mHolder;
+    Camera mCamera;
+
+    public MyCameraSurface(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    // 표면 생성시 카메라 오픈하고 미리보기 설정
+    public void surfaceCreated(SurfaceHolder holder) {
+
+        mCamera = Camera.open();
+        mCamera.setDisplayOrientation(90);
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+        } catch (IOException e) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    // 표면 파괴시 카메라도 파괴한다.
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (mCamera != null) {
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    // 표면의 크기가 결정될 때 최적의 미리보기 크기를 구해 설정한다.
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        Camera.Parameters params = mCamera.getParameters();
+        mCamera.setParameters(params);
+        mCamera.startPreview();
+    }
+}
