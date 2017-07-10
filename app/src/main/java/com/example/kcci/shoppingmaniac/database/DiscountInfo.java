dfa4fa0526d5869548e89b2b1544675eaae65904
KooMyeongHoe <mhk9990@gmail.com>
2017-07-10 20:34:29 +0900

    PriceHistory 추

--- app/src/main/java/com/example/kcci/shoppingmaniac/database/DiscountInfo.java
+++ app/src/main/java/com/example/kcci/shoppingmaniac/database/DiscountInfo.java
@@ -5,78 +5,12 @@ package com.example.kcci.shoppingmaniac.database;
  */
 
 public class DiscountInfo {
-    private String itemId;
-    private String name;
-    private String discountType;
-    private String price;
-    private String discountedPrice;
-    private String category;
-    private String startTime;
-    private String endTime;
-
-    //region getter & setter
-    public String getItemId() {
-        return itemId;
-    }
-
-    void setItemId(String itemId) {
-        this.itemId = itemId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDiscountType() {
-        return discountType;
-    }
-
-    void setDiscountType(String discountType) {
-        this.discountType = discountType;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-    void setPrice(String price) {
-        this.price = price;
-    }
-
-    public String getDiscountedPrice() {
-        return discountedPrice;
-    }
-
-    void setDiscountedPrice(String discountedPrice) {
-        this.discountedPrice = discountedPrice;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getStartTime() {
-        return startTime;
-    }
-
-    void setStartTime(String startTime) {
-        this.startTime = startTime;
-    }
-
-    public String getEndTime() {
-        return endTime;
-    }
-
-    void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
-    //endregion
+    String itemId;
+    public String name;
+    public String discountType;
+    public String price;
+    public String discountedPrice;
+    public String category;
+    public String startTime;
+    public String endTime;
 }
