ed81d2435b562dd3a72ff6c12b32aa007c01bff2
ehdgh9772 <ehdgh9772@naver.com>
2017-07-12 14:50:36 +0900

    Merge remote-tracking branch 'remotes/origin/DB&Server'
    # Conflicts:
    #	app/src/main/res/layout/recycler_view.xml

---
+++ shoppingmanager/src/main/java/com/example/shoppingmanager/Item.java
@@ -0,0 +1,49 @@
+package com.example.shoppingmanager;
+
+public class Item {
+    private int image;
+    private String name;
+    private int price;
+    private String id;
+    private String unit;
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+}
--- shoppingmanager/src/main/java/com/example/shoppingmanager/ListInquiry.java
+++ shoppingmanager/src/main/java/com/example/shoppingmanager/ListInquiry.java
@@ -27,5 +27,51 @@ public class ListInquiry extends AppCompatActivity {
         setContentView(R.layout.activity_list_inquiry);
     }
 
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView picture;
+        public TextView name;
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.activity_list_inquiry, parent, false));
+            picture = (ImageView) itemView.findViewById(R.id.image);
+            name = (TextView) itemView.findViewById(R.id.title);
+        }
+    }
 
+    /**
+     * Adapter to display recycler view.
+     */
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+        // Set numbers of Card in RecyclerView.
+        private static final int LENGTH = 3;
+
+        private final String[] mPlaces;
+        private final Drawable[] mPlacePictures;
+
+        public ContentAdapter(Context context) {
+            Resources resources = context.getResources();
+            mPlaces = resources.getStringArray(R.array.places);
+            TypedArray a = resources.obtainTypedArray(R.array.places_picture);
+            mPlacePictures = new Drawable[a.length()];
+            for (int i = 0; i < mPlacePictures.length; i++) {
+                mPlacePictures[i] = a.getDrawable(i);
+            }
+            a.recycle();
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.picture.setImageDrawable(mPlacePictures[position % mPlacePictures.length]);
+            holder.name.setText(mPlaces[position % mPlaces.length]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return LENGTH;
+        }
+    }
 }
