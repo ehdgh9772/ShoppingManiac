bf49bdc070c8e77bf6b6093f680d5bab7b02cc17
ehdgh9772 <ehdgh9772@naver.com>
2017-07-12 15:36:28 +0900

    Merge remote-tracking branch 'remotes/origin/DB&Server'
    # Conflicts:
    #	app/src/main/res/layout/recycler_view.xml

--- shoppingmanager/src/main/java/com/example/shoppingmanager/ListInquiry.java
+++ shoppingmanager/src/main/java/com/example/shoppingmanager/ListInquiry.java
@@ -1,31 +1,35 @@
 package com.example.shoppingmanager;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.res.Resources;
-import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.AttributeSet;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-public class ListInquiry extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListInquiry extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_inquiry);
+
+        RecyclerView recyclerView=(RecyclerView)findViewById(R.id.my_recycler_view);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(getApplicationContext());
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        List<Item> items=new ArrayList<>();
+        Item[] item=new Item[5];
+//        item[0]=new Item(R.drawable.a,"#1", 2000, "1", "1");
+//        item[1]=new Item(R.drawable.b,"#2");
+//        item[2]=new Item(R.drawable.c,"#3");
+//        item[3]=new Item(R.drawable.d,"#4");
+//        item[4]=new Item(R.drawable.e,"#5");
+
+        for(int i=0;i<5;i++) items.add(item[i]);
+
+        //recyclerView.setAdapter(new MyRecyclerAdapter(getApplicationContext(),albumList,R.layout.activity_list_inquiry));
     }
-
-
 }
