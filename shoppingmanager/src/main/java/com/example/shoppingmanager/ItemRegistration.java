c53ab872085e45226e6eb84e5e343a05946c5a8e
KooMyeongHoe <mhk9990@gmail.com>
2017-07-13 19:32:35 +0900

    카메라 연동동

--- shoppingmanager/src/main/java/com/example/shoppingmanager/ItemRegistration.java
+++ shoppingmanager/src/main/java/com/example/shoppingmanager/ItemRegistration.java
@@ -1,8 +1,9 @@
 package com.example.shoppingmanager;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -23,7 +24,7 @@ public class ItemRegistration extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Intent intent=new Intent(ItemRegistration.this,ImageShooting.class);
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                         startActivity(intent);
 
                         Toast.makeText(getApplicationContext(), "촬영 화면으로 이동", Toast.LENGTH_SHORT).show();
