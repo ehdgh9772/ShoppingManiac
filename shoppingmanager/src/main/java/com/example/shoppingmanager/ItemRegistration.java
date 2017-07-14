4a40b65e046b2b806add514bd7b462d4d7e2b959
ehdgh9772 <ehdgh9772@naver.com>
2017-07-14 10:21:08 +0900

    Manager0.7

--- shoppingmanager/src/main/java/com/example/shoppingmanager/ItemRegistration.java
+++ shoppingmanager/src/main/java/com/example/shoppingmanager/ItemRegistration.java
@@ -30,7 +30,6 @@ public class ItemRegistration extends AppCompatActivity {
         setContentView(R.layout.activity_item_registration);
 
         Button imageShooting = (Button) findViewById(R.id.btn_searchItemId);
-        Button listInquiry = (Button) findViewById(R.id.listInquiry);
         Button registration = (Button) findViewById(R.id.btn_commitItem);
         Database database = new Database();
 
