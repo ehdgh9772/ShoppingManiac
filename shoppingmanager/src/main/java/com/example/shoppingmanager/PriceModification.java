66f6eef33a4348471a6b317b6636eafdd22a0e36
ehdgh9772 <ehdgh9772@naver.com>
2017-07-14 10:09:15 +0900

    Manager0.5

---
+++ shoppingmanager/src/main/java/com/example/shoppingmanager/PriceModification.java
@@ -0,0 +1,13 @@
+package com.example.shoppingmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PriceModification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_price_modification);
+    }
+}
