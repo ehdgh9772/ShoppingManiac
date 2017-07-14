14eaef2075c4820b9a6938e8eba32a3e098bfd12
ehdgh9772 <ehdgh9772@naver.com>
2017-07-14 10:35:50 +0900

    Manager0.8

--- shoppingmanager/src/main/java/com/example/shoppingmanager/PriceModification.java
+++ shoppingmanager/src/main/java/com/example/shoppingmanager/PriceModification.java
@@ -1,7 +1,13 @@
 package com.example.shoppingmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.shoppingmanager.database.Database;
 
 public class PriceModification extends AppCompatActivity {
 
@@ -9,5 +15,38 @@ public class PriceModification extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_price_modification);
+
+        EditText itemid = (EditText) findViewById(R.id.edt_itemId);
+        EditText selectedDate = (EditText) findViewById(R.id.edt_selectedDate);
+        EditText registrationPrice = (EditText) findViewById(R.id.edt_registrationPrice);
+        Button searchItemIdButton = (Button) findViewById(R.id.btn_searchItemId);
+        Button commitButton = (Button) findViewById(R.id.btn_commitEvent);
+
+        final Database database = new Database();
+
+        searchItemIdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SearchItem.class);
+                startActivityForResult(intent, RESULT_OK);
+            }
+        });
+
+        final String date = selectedDate.getText().toString();
+        final String price = registrationPrice.getText().toString();
+
+
+//        commitButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                database.insertDiscountInfo(itemid, date, price,
+//                        new Database.LoadCompleteListener() {
+//                            @Override
+//                            public void onLoadComplete() {
+//
+//                            }
+//                        });
+//            }
+//        });
     }
 }
